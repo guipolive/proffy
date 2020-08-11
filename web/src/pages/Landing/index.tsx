3157c1a5b58db81b2172ef6a32502bf8c56a43d1
Guilherme Oliveira <gui.passosdeoliveira@gmail.com>
2020-08-11 20:27:27 -0300

    create html landing page

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,9 +1,41 @@
 /* nos permite usar JSX */
 import React from 'react';
 
+// Importando as imagens e ícones
+import logoImg from '../../assets/images/logo.svg';
+import landingImg from '../../assets/images/landing.svg';
+import studyIcon from '../../assets/images/icons/study.svg';
+import giveClassesIcon from '../../assets/images/icons/give-classes.svg';
+import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
+
 function Landing(){
     return (
-        <h1>Landing Page</h1>
+        <div id="landing-page">
+            <div id="landing-page-content" className="container">
+                <div className="logo-container">
+                    <img src={logoImg} alt="Proffy"/>
+                    <h2>Sua plataforma de estudos online.</h2>
+                </div>
+
+                <img src={landingImg} alt="" className="hero-image"/>
+
+                <div className="buttons-container">
+                    <a href="#" className="study">
+                        <img src={studyIcon} alt="Estudar"/>
+                        Estudar
+                    </a>
+                    <a href="#" className="give-classes">
+                        <img src={giveClassesIcon} alt="Dar aulas"/>
+                        Dar aulas
+                    </a>
+                </div>
+
+                <span className="total-connections">
+                    <img src={purpleHeartIcon} alt="Coração roxo"/>
+                    Já foram realizadas 200 conexões.
+                </span>
+            </div>
+        </div>
     )
 }
 
